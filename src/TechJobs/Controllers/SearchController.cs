6d4c4647ec2bfea72d7d7088ccfa2fe3e56ade02
Jozel <43223662+Jozel@users.noreply.github.com>
2018-12-10 20:41:30 -0500

    Add existing file

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -4,17 +4,39 @@ using TechJobs.Models;
 
 namespace TechJobs.Controllers
 {
-    public class SearchController : Controller
+    public class SearchController : TechJobs.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
+
             return View();
         }
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> foundJobs = new List<Dictionary<string, string>>();
+
+            if (string.IsNullOrEmpty(searchType) || searchType == "all")
+            {
+                foundJobs = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                foundJobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            ViewBag.title = "Search Results";
+            ViewBag.jobs = foundJobs;
+            ViewBag.searchTerm = searchTerm;
+            ViewBag.searchType = searchType;
+
+            return View("Index");
+        }
 
     }
 }
